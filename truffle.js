7d1ee6175ec27054dbba847b5e2f22d3397dfb7c
nitin <nicky.gurbani@gmail.com>
2018-07-10 17:26:08 +0530

    truffle

--- truffle.js
+++ truffle.js
@@ -1,3 +1,11 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+
+var mnemonic = "<SEED WORDS>";
+
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +13,12 @@ module.exports = {
       port: 8545,
       gas: 1000000014712388,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => {return new HDWalletProvider(mnemonic,"https://rinkeby.infura.io")},
+      port: 8545,
+      network_id: '*' // Match any network id
     }
+
   }
 };
